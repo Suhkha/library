4fa3fcbbc4c3d0a90ea5f3d41c8c78dff4bd66e7
Jessica Villa <jessicavilla@Jessicas-MacBook-Air.local>
2020-07-03 11:54:04 -0500

    add auth validation for routes

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Auth::routes();
 
 
-Route::group(['prefix' => 'panel'], function () {
+Route::group(['middleware' => 'auth', 'prefix' => 'panel'], function () {
   Route::get('/home', 'HomeController@index')->name('home');
 
   Route::prefix('users')->group(function () {
